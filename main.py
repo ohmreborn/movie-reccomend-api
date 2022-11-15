df55f9125b21a77be1fc10d5be9171f79957c803
ohmreborn <98101484+ohmreborn@users.noreply.github.com>
2022-11-15 22:10:27 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # uvicorn main:app --host 165.22.3.172 --port 8000 --reload
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from rec import reccomend,get_title,show_move,get_genres,multi_reccommend
+from rec import reccomend,get_title,get_genres,multi_reccommend,get_by_id
 app = FastAPI()
 
 origins = ["*"]
@@ -16,29 +16,25 @@ app.add_middleware(
 )
 
 @app.get("/")
-async def send_message():
+def send_message():
     return {"message": "Hello Duke"}
     
-@app.get('/chose')
-async def show_movie():
-    return {'data':show_move()}
-
-@app.get('/get-reccom')
-async def get_data(title:str):
-    return {'data':reccomend(title)}
+@app.get('/id')
+def cat_mov(m_id:int):
+    return {'data':get_by_id(m_id)}
 
 @app.get('/get-reccom-2')
-async def get_data_2(title:str):
+def get_data_2(title:str):
     return {'data':multi_reccommend(title)}
-
-@app.get('show-all-genres')
-async def show_genres():
-    return {'data':['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western']}
 
 @app.get('/get-form-genres')
 def genres(gen:str):
     return {'data':get_genres(gen)}
 
 @app.get('/get-title')
-async def get_title_only(title:str):
+def get_title_only(title:str):
     return {'data':get_title(title)}
+
+@app.get('show-all-genres')
+def show_genres():
+    return {'data':['Action', 'Adventure', 'Animation', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 'TV Movie', 'Thriller', 'War', 'Western']}
--- rec.py
+++ rec.py
@@ -2,7 +2,47 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import requests
-def reccomend(title):
+class reccomend():
+    def __init__(self):
+        self.df = pd.read_csv('movies.csv')
+        vec = CountVectorizer()
+        self.x = vec.fit(self.df['genre_ids'])
+        self.y = self.x.transform(self.df['genre_ids'])
+
+    def rank(self,score,m_id=None):
+        self.df['score'] = score
+
+        if m_id is not  None:
+            self.df.drop(m_id,axis=0,inplace=True)
+
+        self.df.sort_values(by='score',inplace=True,ascending=False)
+        self.df = self.df.head(10)
+        self.df = self.df.reset_index(drop=False)
+        data = []
+        for i in range(10):
+            
+            data.append(self.df.loc[i].to_dict())
+        return data
+
+def get_by_id(mov_id,init=reccomend()):
+    df = init.df
+    movie = df[df['id'] == int(mov_id)].index.item()
+    score = cosine_similarity(init.y,init.y[movie])
+
+    return init.rank(score,[movie])
+
+
+def multi_reccommend(m_id,init=reccomend()):
+    df = init.df
+    movie = []
+    for m in m_id.split(','):
+        i = df[df['id'] == int(m)].index.item()
+        movie.append(i)
+    u_mov = init.y[movie].sum(axis=0).T
+    score = init.y @ u_mov
+    return init.rank(score,movie)
+
+def get_title(title,init=reccomend()):
     url = f"https://api.themoviedb.org/3/search/movie?api_key=0b4a78f3f6df40ca3779248e701f90e5&language=en-US&query={title}&page=1&include_adult=false"
 
     payload={}
@@ -13,113 +53,26 @@ def reccomend(title):
     results = response.json()
     if results['total_results'] == 0 :
         return 'there are no movie in this api'
-    results = results['results'][0]['original_title']
-
-
+    results = results['results'][0]['id']
     
-    df = pd.read_csv('movies.csv')
-    # df = df[['genre_ids','original_title']]
-    movie = df[df['original_title'] == results ]['original_title'].index.item()
+    score = cosine_similarity(init.y,init.y[results])
+    return pd.DataFrame(init.rank(score))['original_title'].tolist()
 
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(df['genre_ids'])
-    score = cosine_similarity(X,X[movie])
-
-    df['score'] = score
-    df.drop([movie],axis=0,inplace=True)
-    df.sort_values(by='score',inplace=True,ascending=False)
-    df = df.head(10)
-    df = df.reset_index(drop=False)
-    data = []
-    for i in range(10):
-        
-        data.append(df.loc[i].to_dict())
-    return data
-
-def multi_reccommend(title):
-    total_results = 0
-    all_movies = []
-    for m in title.split(','):
-        url = f"https://api.themoviedb.org/3/search/movie?api_key=0b4a78f3f6df40ca3779248e701f90e5&language=en-US&query={m}&page=1&include_adult=false"
-
-        payload={}
-        headers = {}
-
-        response = requests.request("GET", url, headers=headers, data=payload)
-
-        results = response.json()
-        total_results += int(results['total_results'])
-        results = results['results'][0]['original_title']
-        all_movies.append(results)
-
-    if total_results == 0 :
-            return 'there are no movie in this api'
-
-    df = pd.read_csv('movies.csv')
-        
-    movies = []
-    for i in all_movies:
-        movie = df[df['original_title'] == i].index.tolist()[0]
-        movies.append(movie)
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit_transform(df['genre_ids']) 
-    genres = X[movies].toarray().sum(axis=0).reshape(-1,1)
-    df['score'] = X @ genres
-    df.drop(movies,axis=0,inplace=True)
-    df.sort_values(by='score',inplace=True,ascending=False)
-    df = df.head(10)
-    df = df.reset_index(drop=False)
-    data = []
-    for i in range(10):
-        
-        data.append(df.loc[i].to_dict())
-    return data
-
-def get_title(title):
-    data = reccomend(title)
-    if data == 'there are no movie in this api':
-        return data
-    
-    movie = []
-    for i in data:
-        movie.append(i['original_title'])
-    return movie
-
-def get_genres(genres:str):
+def get_genres(genres:str,init=reccomend()):
     gen = {'Action': '28', 'Adventure': '12', 'Animation': '16', 'Comedy': '35', 'Crime': '80', 'Documentary': '99', 'Drama': '18', 'Family': '10751', 'Fantasy': '14', 'History': '36', 'Horror': '27', 'Music': '10402', 'Mystery': '9648', 'Romance': '10749', 'Science Fiction': '878', 'TV Movie': '10770', 'Thriller': '53', 'War': '10752', 'Western': '37'}
     genres = genres.split(',')
     inp_gen = []
     for i in genres:
         inp_gen.append(gen[i])
-    inp_gen = ",".join(inp_gen)
+    inp_gen = [",".join(inp_gen)]
+    data = init.x.transform(inp_gen)
+    score = cosine_similarity(init.y,data)
 
-    df = pd.read_csv('movies.csv')
+    return init.rank(score)
 
-    vectorizer = CountVectorizer()
-    X = vectorizer.fit(df['genre_ids'])
-    data = X.transform(df['genre_ids'])
-    movie = X.transform([inp_gen])
-    score = cosine_similarity(data,movie)
 
-    df['score'] = score
-    df.sort_values(by='score',inplace=True,ascending=False)
-    df = df.reset_index(drop=True)
-    df = df.iloc[:10]
-    
-    data = []
-    for i in range(10):
-        
-        data.append(df.loc[i].to_dict())
-
-    return data
-
-def show_move():
-    df = pd.read_csv('movies.csv').head()
-    data = []
-    for i in range(5):
-        data.append(df.loc[i].to_dict())
-
-    return data
 if __name__ == "__main__":
-    print(multi_reccommend('thor,the avenger'))
+    # print(multi_reccommend('167032,16871',reccomend()))
+    print(get_title('the avenger',reccomend()))
+    # print(get_genres('Action,Adventure',reccomend()))
     
